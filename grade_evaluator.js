4570b5ffc09855724deefa0285edee84a8f9ce54
ayanahmed2f4f-pixel <ayanahmed2f4f@gmail.com>
2026-02-28 10:28:46 -0600

    Fix function name and log students correctly

--- grade_evaluator.js
+++ grade_evaluator.js
@@ -1,1 +1,19 @@
+const students = [{
+  name: "marcus", 
+  grade: 85,
+}, { 
+  name: "liam", 
+  grade: 80,
+}, {
+  name: "Ayan",
+  grade: 90,
+}, {
+  name: "Abdul",
+  grade: 75,
+}];
 
+function evaluateGrade (){
+  console.log (students);
+}
+
+evaluateGrade ();
