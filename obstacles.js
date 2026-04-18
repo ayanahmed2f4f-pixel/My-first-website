f0a290adb2c0e2e733c9a372ca2087ae248ad2cc
ayanahmed2f4f-pixel <ayanahmed2f4f@gmail.com>
2026-04-18 11:43:22 -0500

    Increase scroll speed from 2 to 5

--- obstacles.js
+++ obstacles.js
@@ -12,7 +12,7 @@ let isInvisible = false;
 function init() {
   spawnTimer = 0;
   spawnRate = 150;
-  scrollSpeed = 2;
+  scrollSpeed = 5;
   gap = 200; // space between top and bottom pipes
   score = 0;
   newScore = 0;
